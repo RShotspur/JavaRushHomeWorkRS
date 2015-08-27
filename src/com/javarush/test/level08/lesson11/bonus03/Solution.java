2fc758a9df98b160c74c7407c27d6091cab1fc86
RS <rs.hotspur@gmail.com>
2015-08-27 15:37:00 +0300

    New changelist

--- src/com/javarush/test/level08/lesson11/bonus03/Solution.java
+++ src/com/javarush/test/level08/lesson11/bonus03/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.test.level08.lesson11.bonus03;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Date;
 
 /* Задача по алгоритмам
 Задача: Введи с клавиатуры 20 слов и выведи их в алфавитном порядке.
@@ -15,10 +16,12 @@ public class Solution
         String[] array = new String[20];
         for (int i = 0; i < array.length; i++)
         {
-            array[i] = reader.readLine();
+            array[i] = "xfgnlcjkncn" + i;
         }
-
+        long date1 = new Date().getTime();
         sort(array);
+        long date2 = new Date().getTime();
+        System.out.println(date2 - date1);
 
         for (String x : array)
         {
@@ -29,7 +32,19 @@ public class Solution
     public static void sort(String[] array)
     {
         //напишите тут ваш код
-
+//        for (int i=0; i<array.length-1;){
+//            int s = i+1;
+//            String temp = null;
+//            //сначала отправляем первое и второе значение на сверку
+//            if(isGreaterThan(array[i], array[s])){
+//                temp = array[s];
+//                array[s]=array[i];
+//                array[i]=temp;
+//                if (i>0) i--;
+//            }
+//            else i++;
+//
+//        }
         String temp;
         for (int i = 0; i < array.length; i++)
         {
